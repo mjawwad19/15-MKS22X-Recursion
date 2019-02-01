16aa37524db2e4c55ae0aa89439965808468c98e
Mohammad Jawwad <mjawwad@stuy.edu>
2019-02-01 02:23:53 -0500

    Updated sqrt method to have tolerance param, updated their documentation to be in java doc.

--- recursion.java
+++ recursion.java
@@ -4,25 +4,27 @@ public class recursion{
       *Recursively find the sqrt using Newton's approximation
       *tolerance is the allowed percent error the squared answer is away from n.
       *precondition: n is non-negative
-      */
-
-    public static double sqrt(double n){
+      *@param n is the number we are trying to sqrt
+      *@param tolerance is the limit our sqrt can be inaccurate
+      *@return the sqrt with a percent error less than tolerance.
+    */
+    public static double sqrt(double n, double tolerance){
       if (n == 0) { return 0; //could be moved down to helper tbh
-      }else {
-        return sqrtH(n, n/2);
+      }else{
+        return sqrtH(n, n/2, tolerance);
       }
     }
 
-    /*
+    /**
     *@param n any non-negative non-zero value you want to take the sqrt of
     *@param guess is the guessed sqrt that recursively updates to get closer to real sqrt.
-    *@return the approximate sqrt of n within a tolerance of 0.001%
+    *@param tolerance is the limit to how inaccurate the guess can be.
+    *@return the approximate sqrt of n within a tolerance
     */
-    private static double sqrtH(double n, double guess){
-      if (Math.abs(Math.pow(guess, 2) - n) / n > .00001) {
+    private static double sqrtH(double n, double guess, double tolerance){
+      if (Math.abs(Math.pow(guess, 2) - n) / n > tolerance) {
         return sqrtH(n, (n / guess + guess) / 2);
-      }
-      else {
+      }else {
         return guess;
       }
     }
